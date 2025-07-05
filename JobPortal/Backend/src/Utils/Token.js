922ff3d5748acad213932a6edd6eb30e57a63bd6
BubaiShil <bubaishil56@gmail.com>
2025-07-06 01:37:14 +0530

    adj9

--- JobPortal/Backend/src/Utils/Token.js
+++ JobPortal/Backend/src/Utils/Token.js
@@ -15,7 +15,8 @@ export const getToken = (userId, res) => {
     res.cookie('jwt', token, {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite : "strict",
+        // sameSite : "strict",
+        sameSite : process.env.NODE_ENV === "production" ? "None" : "Lax", 
         secure: process.env.NODE_ENV !== "development",
     });
 
