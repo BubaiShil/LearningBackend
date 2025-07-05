0ac6167edb699349ded19b3f5ac83ff5b200ab18
BubaiShil <bubaishil56@gmail.com>
2025-07-05 20:13:24 +0530

    done adjustments pt3

--- JobPortal/Frontend/src/lib/axios.js
+++ JobPortal/Frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000/api/v1",
+  baseURL: API_BASE_URL,
   withCredentials: true,
 });
